ae38c47cf9a120d62aeff35733da93e996229f17
Renato Eustaquio Alves <blackangeum@hotmail.com>
2018-08-03 01:10:03 -0300

    Classe Modelo marca criada

---
+++ BetacubeERP/Modelo/ModMarca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ModMarca
+    {
+        private int id;
+        private string descricao;
+
+        public int Id { get => id; set => id = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+    }
+}
